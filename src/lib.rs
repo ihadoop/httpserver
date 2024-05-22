c5afa564d20c7c57b3994a764222104e7e2a8435
13574798@qq.com <13574798@qq.com>
2024-05-22 21:22:29 +0800

    11

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-pub struct  ThreadPool;
+pub struct  ThreadPool{
+    threads:Vec<std::thread::JoinHandle<()>>
+}
 
 
 impl ThreadPool{
 
     pub fn new(num:usize)->ThreadPool{
-        ThreadPool
+        let mut threads = Vec::with_capacity(num);
+        for _ in 0..num {
+            // create some threads and store them in the vector
+        }
+        ThreadPool {threads}
     }
 
     pub fn execute<F>(&self, f: F)
